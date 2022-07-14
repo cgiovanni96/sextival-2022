7921422e7b03ee8889bc85e15aa15d3ee88dff59
Giovanni Costa <cgiovanni1996@gmail.com>
2022-07-14 16:10:17 +0200

    :sparkles: [v2]feat(theme): emotion cache

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,16 @@ import type { AppProps } from "next/app";
 
 import { mantine } from "@sextival/theme/mantine";
 import Header from "@sextival/components/Header";
+import cache from "@sextival/theme/cache";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={mantine}>
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={mantine}
+      emotionCache={cache}
+    >
       <AppShell
         sx={{
           main: {
---
+++ v2/theme/cache.ts
@@ -0,0 +1,6 @@
+// rtl-cache.ts
+import { createEmotionCache } from "@mantine/core";
+
+export default createEmotionCache({
+  key: "mantine-rtl",
+});
