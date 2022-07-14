eefb7c4043ed15b44602261ec8bb4efdc9e338d1
Giovanni Costa <cgiovanni1996@gmail.com>
2022-07-14 18:11:25 +0200

    :sparkles: [v2]feat(content): content section

--- v2/modules/home/Content/Content.tsx
+++ v2/modules/home/Content/Content.tsx
@@ -1,14 +1,70 @@
+import { useRouter } from "next/router";
+import { AspectRatio, BackgroundImage, SimpleGrid, Title } from "@mantine/core";
+import { motion } from "framer-motion";
+
 import Panel from "@sextival/components/Panel";
-import { DivRef } from "@sextival/types/react";
+import { shadows } from "@sextival/theme/shadows";
 
 type Props = {
   order: number;
 };
 
+const Data = [
+  {
+    img: "/v2/home/content/home-sextival.png",
+    text: "Il Sextival",
+    to: "/il-sextival",
+  },
+  { img: "/v2/home/content/home-nassa.png", text: "La Nassa", to: "/la-nassa" },
+  { img: "/v2/home/content/home-ospiti.png", text: "Lə ospitə", to: "/ospiti" },
+];
+
 const Content = ({ order = 3 }: Props) => {
+  const router = useRouter();
+
   return (
     <Panel order={order} scrollDown sx={{ marginTop: "5px" }}>
-      <span>Hello</span>
+      <SimpleGrid
+        cols={1}
+        spacing="lg"
+        breakpoints={[{ minWidth: 800, cols: 3, spacing: "md" }]}
+        mt={80}
+      >
+        {Data.map((d, i) => (
+          <motion.div
+            key={i}
+            initial={{
+              y: 200,
+              opacity: 0,
+            }}
+            whileInView={{
+              y: 0,
+              opacity: 1,
+              transition: { delay: 0.1 * i },
+            }}
+            viewport={{ once: true, amount: 0.6 }}
+          >
+            <AspectRatio
+              ratio={16 / 9}
+              sx={{ cursor: "pointer", boxShadow: shadows.box }}
+              onClick={() => {
+                router.push(d.to);
+              }}
+            >
+              <BackgroundImage src={d.img} sx={{ borderRadius: 8 }} />
+              <Title
+                sx={{
+                  color: "white",
+                  marginTop: "auto",
+                  textShadow: "0 0 4px rgba(0,0,0,.6)",
+                }}
+              >
+                {d.text}
+              </Title>
+            </AspectRatio>
+          </motion.div>
+        ))}
+      </SimpleGrid>
     </Panel>
   );
 };
